0b1f92498c5e31600608a699251e69f66532ce1f
Channely <qijie29896@gmail.com>
2016-08-01 18:04:34 +0800

    add the ajax demo method for node side

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'JAM' });
+});
+
+/* Ajax GET. /example */
+router.get('/example', function(req, res) {
+  res.send({ title: 'JAM' });
+});
+
+/* Ajax POST. /example */
+router.post('/example', function(req, res) {
+  res.send({ title: 'JAM' });
 });
 
 module.exports = router;
